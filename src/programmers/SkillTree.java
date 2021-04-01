654eee0a0e811d2a9652e46a1409fd03f567c2bb
지영진 <wldudwls12@gmail.com>
2021-04-01 15:26:51 +0900

    새로운 풀이법 참고

--- src/programmers/SkillTree.java
+++ src/programmers/SkillTree.java
@@ -24,23 +24,39 @@ public class SkillTree {
     }
 	
 	public boolean isPossible(String skill, String skillTree) {
-		boolean possible = true;
-		
-		int prevIdx = skillTree.indexOf(skill.charAt(0));
-		int curIdx = 0;
-		for(int i=1; i<skill.length(); i++) {
-			char c = skill.charAt(i);
-			curIdx = skillTree.indexOf(c);
-			if(
-					(prevIdx>curIdx) && (curIdx != -1) // skill이 있긴 하지만 순서가 잘못된 경우
-				  || (prevIdx==-1 && curIdx != -1) // 중간이 빈 경우
-				) 
-			{
-				return false;
+		boolean possible = false;
+		String skills = skillTree;
+		for(int i=0; i<skillTree.length(); i++) {
+			String s = String.valueOf(skillTree.charAt(i));
+			if(!skill.contains(s)) {
+				skills = skills.replace(s,  "");
 			}
-			prevIdx = curIdx;
+		}
+		if(skill.indexOf(skills) == 0) {
+			return true;
 		}
 		
 		return possible;
 	}
+	
+//	public boolean isPossible(String skill, String skillTree) {
+//		boolean possible = true;
+//		
+//		int prevIdx = skillTree.indexOf(skill.charAt(0));
+//		int curIdx = 0;
+//		for(int i=1; i<skill.length(); i++) {
+//			char c = skill.charAt(i);
+//			curIdx = skillTree.indexOf(c);
+//			if(
+//					(prevIdx>curIdx) && (curIdx != -1) // skill이 있긴 하지만 순서가 잘못된 경우
+//				  || (prevIdx==-1 && curIdx != -1) // 중간이 빈 경우
+//				) 
+//			{
+//				return false;
+//			}
+//			prevIdx = curIdx;
+//		}
+//		
+//		return possible;
+//	}
 }
