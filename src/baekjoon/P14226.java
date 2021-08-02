570051a47f22a9fff091c701051d17a9252aa6ee
Ganjinajae <wldudwls12@gmail.com>
2021-08-03 00:33:27 +0900

    첫 번째 풀이 메모리초과

--- src/baekjoon/P14226.java
+++ src/baekjoon/P14226.java
@@ -18,7 +18,53 @@ public class P14226 {
 		
 		int S = Integer.parseInt(br.readLine());
 		
+		Queue<Screen> q = new LinkedList<>();
+		q.add(new Screen(0, 1));
+		int answer = 0;
+		boolean exitFlag = false;
+		while(!q.isEmpty()) {
+			int n = q.size();
+			while(n-- > 0) {
+				Screen s = q.poll();
+				if(s.getNum() == S) {
+					exitFlag = true;
+					break;
+				}
+				q.add(new Screen(s.getNum(), s.getNum()));
+				if(s.getClipboard() != 0) {
+					q.add(new Screen(s.getNum(), s.getNum()+s.getClipboard()));
+				}
+				if(s.getNum() > 0) {
+					q.add(new Screen(s.getClipboard(), s.getNum()-1));
+				}
+			}
+			if(exitFlag) break;
+			answer++;
+		}
 		
-		
+		System.out.println(answer);
+	}
+}
+
+class Screen {
+	int clipboard;
+	int num = 1;
+	
+	Screen(int clipboard, int num) {
+		this.clipboard = clipboard;
+		this.num = num;
+	}
+	
+	public int getClipboard() {
+		return clipboard;
+	}
+	public void setClipboard(int clipboard) {
+		this.clipboard = clipboard;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
 	}
 }
