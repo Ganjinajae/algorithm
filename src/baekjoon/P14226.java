f3d9692a8abb7633f2d3144cbfd2887199bd8d40
Ganjinajae <wldudwls12@gmail.com>
2021-08-03 00:50:01 +0900

    방문여부를 판단해서 그래프 탐색하는 풀이 추가(중요!)

--- src/baekjoon/P14226.java
+++ src/baekjoon/P14226.java
@@ -12,6 +12,8 @@ import java.util.Queue;
  *
  */
 public class P14226 {
+	
+	static boolean[][] visited = new boolean[1001][1001];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,40 +21,38 @@ public class P14226 {
 		int S = Integer.parseInt(br.readLine());
 		
 		Queue<Screen> q = new LinkedList<>();
-		q.add(new Screen(0, 1));
-		int answer = 0;
-		boolean exitFlag = false;
+		q.add(new Screen(0, 1, 0));
+		visited[0][1] = true;
 		while(!q.isEmpty()) {
-			int n = q.size();
-			while(n-- > 0) {
-				Screen s = q.poll();
-				if(s.getNum() == S) {
-					exitFlag = true;
-					break;
-				}
-				q.add(new Screen(s.getNum(), s.getNum()));
-				if(s.getClipboard() != 0) {
-					q.add(new Screen(s.getNum(), s.getNum()+s.getClipboard()));
-				}
-				if(s.getNum() > 0) {
-					q.add(new Screen(s.getClipboard(), s.getNum()-1));
-				}
+			
+			Screen s = q.poll();
+			if(s.getNum() == S) {
+				System.out.println(s.getSecond());
+				break;
 			}
-			if(exitFlag) break;
-			answer++;
+			
+			q.add(new Screen(s.getNum(), s.getNum(), s.getSecond()+1));
+			if(s.getClipboard() != 0 && s.getNum()+s.clipboard <= S && !visited[s.getClipboard()][s.getNum()+s.clipboard]) {
+				q.add(new Screen(s.getClipboard(), s.getNum()+s.clipboard, s.getSecond()+1));
+				visited[s.getClipboard()][s.getNum()+s.clipboard] = true;
+			}
+			if(s.getNum()-1 >=0 && !visited[s.getClipboard()][s.getNum()-1]) {
+				q.add(new Screen(s.getClipboard(), s.getNum()-1, s.getSecond()+1));
+				visited[s.getClipboard()][s.getNum()-1] = true;
+			}
 		}
-		
-		System.out.println(answer);
 	}
 }
 
 class Screen {
 	int clipboard;
-	int num = 1;
+	int num;
+	int second;
 	
-	Screen(int clipboard, int num) {
+	Screen(int clipboard, int num, int second) {
 		this.clipboard = clipboard;
 		this.num = num;
+		this.second = second;
 	}
 	
 	public int getClipboard() {
@@ -66,5 +66,11 @@ class Screen {
 	}
 	public void setNum(int num) {
 		this.num = num;
+	}
+	public int getSecond() {
+		return this.second;
+	}
+	public void setSecond(int second) {
+		this.num = second;
 	}
 }
