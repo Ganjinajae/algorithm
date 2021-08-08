ac49a2da40c0879b7fc4f5a12c4b5eff0ca0dfd1
Ganjinajae <wldudwls12@gmail.com>
2021-08-08 16:37:04 +0900

    예상 대진표 풀이추가(O(1) 풀이)

--- src/programmers/ExpectationDraw.java
+++ src/programmers/ExpectationDraw.java
@@ -28,33 +28,12 @@ public class ExpectationDraw {
 			list.add(i);
 		}
 
-		boolean flag = false;
-		while(true) {
-			List<Integer> temp = new ArrayList<>();
-			for(int i=0; i<list.size()-1; i+=2) {
-				if((list.get(i) == a && list.get(i+1) == b)
-						|| (list.get(i) == b && list.get(i+1) == a)) {
-					flag = true;
-					break;
-				}
-				else {
-					if(list.get(i) == a || list.get(i) == b) {
-						temp.add(list.get(i));
-					}
-					else if(list.get(i+1) == a || list.get(i+1) == b) {
-						temp.add(list.get(i+1));
-					}
-					else {
-						temp.add(0);
-					}
-				}
-			}
+		while(a!=b) {
+			a = a/2 + a%2;
+			b = b/2 + b%2;
 			answer++;
-			if(flag) {
-				break;
-			}
-			list = temp;
 		}
+		
 		return answer;
 	}
 }
