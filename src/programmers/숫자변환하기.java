79d60ea241420d5d3abd3ac59422f81f14874577
Ganjinajae <wldudwls12@gmail.com>
2024-05-09 23:07:37 +0900

    숫자변환하기 풀이 추가(dp, bottom-up)

--- src/programmers/숫자변환하기.java
+++ src/programmers/숫자변환하기.java
@@ -13,37 +13,75 @@ public class 숫자변환하기 {
 	}
 	
 	
+	/**
+	 * DP 풀이
+	 * @param x
+	 * @param y
+	 * @param n
+	 * @return
+	 */
 	public int solution(int x, int y, int n) {
-        Set<Integer> set = new HashSet<>();
-        set.add(x);
+        int[] dp = new int[1_000_001];
         
-        int answer = 0;
-        Iterator<Integer> iter = set.iterator();
-        while(iter.hasNext()) {
-        	Set<Integer> tempSet = new HashSet<>();
-        	while (iter.hasNext()) {
-        		int num = iter.next();
-            	if (num == y) {
-            		return answer;
-            	}
-        		if (num + n <= y) {
-            		tempSet.add(num + n);
-            	}
-            	if (num*2 <= y) {
-            		tempSet.add(num*2);
-            	}
-            	if (num*3 <= y) {
-            		tempSet.add(num*3);
-            	}
+        for (int i=x; i<=y; i++) {
+        	if (i!=x && dp[i] == 0) {
+        		dp[i] = -1;
+        		continue;
         	}
-        	
-        	answer++;
-        	iter = tempSet.iterator();
+        	if (i+n <= y) {
+        		dp[i+n] = dp[i+n] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i+n]);
+        	}
+        	if (i*2 <= y) {
+        		dp[i*2] = dp[i*2] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*2]);
+        	}
+        	if (i*3 <= y) {
+        		dp[i*3] = dp[i*3] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*3]);
+        	}
         }
         
-        
-        return -1;
+        return dp[y];
     }
+	
+	
+	
+	/**
+	 * BFS 풀이
+	 * @param x
+	 * @param y
+	 * @param n
+	 * @return
+	 */
+//	public int solution(int x, int y, int n) {
+//        Set<Integer> set = new HashSet<>();
+//        set.add(x);
+//        
+//        int answer = 0;
+//        Iterator<Integer> iter = set.iterator();
+//        while(iter.hasNext()) {
+//        	Set<Integer> tempSet = new HashSet<>();
+//        	while (iter.hasNext()) {
+//        		int num = iter.next();
+//            	if (num == y) {
+//            		return answer;
+//            	}
+//        		if (num + n <= y) {
+//            		tempSet.add(num + n);
+//            	}
+//            	if (num*2 <= y) {
+//            		tempSet.add(num*2);
+//            	}
+//            	if (num*3 <= y) {
+//            		tempSet.add(num*3);
+//            	}
+//        	}
+//        	
+//        	answer++;
+//        	iter = tempSet.iterator();
+//        }
+//        
+//        
+//        return -1;
+//    }
 	
 //	private static int MIN_NUMBER = Integer.MAX_VALUE;
 	
