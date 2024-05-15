4405f2487f2fe67772cb988d773481fe8052076b
Ganjinajae <wldudwls12@gmail.com>
2024-05-15 23:57:27 +0900

    숫자변환하기 dp(top-down) 풀이추가
    * 재귀 깊이가 깊어져서 stackoverflow로 runtime error 예상된다. 80점짜리 답

--- src/programmers/숫자변환하기.java
+++ src/programmers/숫자변환하기.java
@@ -1,7 +1,9 @@
 package programmers;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 public class 숫자변환하기 {
@@ -13,34 +15,91 @@ public class 숫자변환하기 {
 	}
 	
 	
+	
 	/**
-	 * DP 풀이
+	 * DP 풀이 (top-down)
 	 * @param x
 	 * @param y
 	 * @param n
 	 * @return
 	 */
 	public int solution(int x, int y, int n) {
-        int[] dp = new int[1_000_001];
-        
-        for (int i=x; i<=y; i++) {
-        	if (i!=x && dp[i] == 0) {
-        		dp[i] = -1;
-        		continue;
-        	}
-        	if (i+n <= y) {
-        		dp[i+n] = dp[i+n] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i+n]);
-        	}
-        	if (i*2 <= y) {
-        		dp[i*2] = dp[i*2] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*2]);
-        	}
-        	if (i*3 <= y) {
-        		dp[i*3] = dp[i*3] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*3]);
-        	}
-        }
-        
-        return dp[y];
+        return dp(x, y, n , new HashMap<>());
     }
+	
+	private int dp(int x, int y, int n, Map<Integer, Integer> memo) {
+		// base
+		if (x == y) {
+			return 0;
+		}
+		if (x > y) {
+			return -1;
+		}
+		
+		// memo
+		if (memo.containsKey(x)) {
+			return memo.get(x);
+		}
+		
+		// recursive
+		int minOperation = Integer.MAX_VALUE;
+		if (x*3 <= y) {
+			int operation = dp(x*3, y, n, memo);
+			if (operation != -1) {
+				minOperation = Math.min(minOperation, operation + 1);
+			}
+		}
+		if (x*2 <= y) {
+			int operation = dp(x*2, y, n, memo);
+			if (operation != -1) {
+				minOperation = Math.min(minOperation, operation + 1);
+			}	
+		}
+		if (x+n <= y) {
+			int operation = dp(x+n, y, n, memo);
+			if (operation != -1) {
+				minOperation = Math.min(minOperation, operation + 1);
+			}
+		}
+
+		
+		if (minOperation == Integer.MAX_VALUE) {
+			minOperation = -1;
+		}
+		memo.put(x, minOperation);
+
+		return minOperation;
+	}
+	
+	
+	/**
+	 * DP 풀이 (bottmp-up)
+	 * @param x
+	 * @param y
+	 * @param n
+	 * @return
+	 */
+//	public int solution(int x, int y, int n) {
+//        int[] dp = new int[1_000_001];
+//        
+//        for (int i=x; i<=y; i++) {
+//        	if (i!=x && dp[i] == 0) {
+//        		dp[i] = -1;
+//        		continue;
+//        	}
+//        	if (i+n <= y) {
+//        		dp[i+n] = dp[i+n] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i+n]);
+//        	}
+//        	if (i*2 <= y) {
+//        		dp[i*2] = dp[i*2] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*2]);
+//        	}
+//        	if (i*3 <= y) {
+//        		dp[i*3] = dp[i*3] == 0 ? dp[i]+1 : Math.min(dp[i]+1, dp[i*3]);
+//        	}
+//        }
+//        
+//        return dp[y];
+//    }
 	
 	
 	
